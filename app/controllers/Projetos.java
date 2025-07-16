2ae8a89f88bff3d805b1688375b6011869fd9f38
Lucas Dantas <lucas.dantas@ifrn.edu.br>
2025-07-16 11:47:25 -0300

    Refatoração: especializa controlador para tratar associação de membros ao projeto

--- app/controllers/Projetos.java
+++ app/controllers/Projetos.java
@@ -13,44 +13,4 @@ public class Projetos extends Controller {
 		List<Projeto> projetos = Projeto.findAll();
 		render(projetos);
 	}
-	
-	public static void formMembro(Long id) {
-		Projeto projeto = Projeto.findById(id);
-		List<Pessoa> pessoas = Pessoa.findAll();
-		render(projeto, pessoas);
-	}
-	
-	public static void salvarMembro(Long pessoaId, Long projetoId) {
-		Pessoa pessoa = Pessoa.findById(pessoaId);
-		Projeto projeto = Projeto.findById(projetoId);
-		
-		if (projeto.membros == null) {
-			projeto.membros = new ArrayList<Pessoa>();
-		}
-		
-		if (projeto.membros.contains(pessoa)) {
-			flash.error("Essa pessoa já é membro do projeto!");
-			formMembro(projetoId);
-		}
-		
-		projeto.membros.add(pessoa);
-		projeto.save();
-		formMembro(projetoId);
-	}
-	
-	public static void removerMembro(Long projetoId, Long pessoaId) {
-		Pessoa pessoa = Pessoa.findById(pessoaId);
-		Projeto projeto = Projeto.findById(projetoId);
-		
-		if (!projeto.membros.contains(pessoa)) {
-			flash.error("Essa pessoa não é membro do projeto!");
-			formMembro(projetoId);
-		}
-		
-		projeto.membros.remove(pessoa);
-		projeto.save();
-		flash.success("Pessoa removida com sucesso!");
-		formMembro(projetoId);
-	}
-
 }
---
+++ app/controllers/ProjetosMembros.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Pessoa;
+import models.Projeto;
+import play.mvc.Controller;
+
+public class ProjetosMembros extends Controller {
+	
+	public static void form(Long id) {
+		Projeto projeto = Projeto.findById(id);
+		List<Pessoa> pessoas = Pessoa.findAll();
+		render(projeto, pessoas);
+	}
+	
+	public static void salvar(Long pessoaId, Long projetoId) {
+		Pessoa pessoa = Pessoa.findById(pessoaId);
+		Projeto projeto = Projeto.findById(projetoId);
+		
+		if (projeto.membros == null) {
+			projeto.membros = new ArrayList<Pessoa>();
+		}
+		
+		if (projeto.membros.contains(pessoa)) {
+			flash.error("Essa pessoa já é membro do projeto!");
+			form(projetoId);
+		}
+		
+		projeto.membros.add(pessoa);
+		projeto.save();
+		form(projetoId);
+	}
+	
+	public static void remover(Long projetoId, Long pessoaId) {
+		Pessoa pessoa = Pessoa.findById(pessoaId);
+		Projeto projeto = Projeto.findById(projetoId);
+		
+		if (!projeto.membros.contains(pessoa)) {
+			flash.error("Essa pessoa não é membro do projeto!");
+			form(projetoId);
+		}
+		
+		projeto.membros.remove(pessoa);
+		projeto.save();
+		flash.success("Pessoa removida com sucesso!");
+		form(projetoId);
+	}
+
+}
